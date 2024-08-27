9316d36aec78a27934118b04b27a678ee1a0fe81
gesielbr <gesiel.br@gmail.com>
2024-08-27 20:06:53 -0300

    buttons next and previous

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularMultiStepForms';
+
+  step: any = 1;
+
+  btnNext() {
+    this.step += 1;
+  }
+
+  btnPrevious() {
+    this.step -= 1;
+  }
 }
