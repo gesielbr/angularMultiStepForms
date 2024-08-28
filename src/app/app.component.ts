947068159804d67b33d927188b4bed588d16cf76
gesielbr <gesiel.br@gmail.com>
2024-08-28 19:41:22 -0300

    FormGroup with constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  FormGroup,
+  ReactiveFormsModule,
+  FormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angularMultiStepForms';
 
+  myForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      userDetails: formBuilder.group({
+        fname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      }),
+      additionalDetails: formBuilder.group({
+        mobile: ['', [Validators.required, Validators.maxLength(10)]],
+        address: ['', Validators.required],
+        country: ['', Validators.required],
+        gender: ['', Validators.required],
+      }),
+      feedback: formBuilder.group({
+        comments: [''],
+      }),
+    });
+  }
   step: any = 1;
 
   btnNext() {
@@ -20,5 +46,9 @@ export class AppComponent {
 
   btnPrevious() {
     this.step -= 1;
+  }
+
+  formSubmit() {
+    console.log(this.myForm.value);
   }
 }
